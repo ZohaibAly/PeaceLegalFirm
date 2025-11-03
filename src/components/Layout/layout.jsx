0d44de3a47eeae8f33197bf223a0b24b40ddb5cb
[zohaib <[mzohaibali223@gmail.com]>
2025-11-04 01:36:47 +0500

    add animation as well as apply lazy loader

--- src/components/Layout/layout.jsx
+++ src/components/Layout/layout.jsx
@@ -2,17 +2,21 @@
 import React from "react";
 import Navbar from "../Navbar";
 //import Footer from "../Footer";
-// import ScrollToTop from "../ScrollToTop";
+import LazyWrapper from "../LazyWrapper/LazyWrapper";
+import ScrollToTop from "../STT";
 import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
     <>
+    <LazyWrapper>
       <Navbar />
+      <ScrollToTop/>
       <main>
         <Outlet />
       </main>
       {/* <Footer /> */}
+      </LazyWrapper>
     </>
   );
 };
