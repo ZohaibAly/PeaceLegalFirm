eca141b923fcfcfb80dc900300fc24c818e50010
[zohaib <[mzohaibali223@gmail.com]>
2025-11-03 20:26:54 +0500

    fix

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -78,10 +78,11 @@ const HeroSection = () => {
 
       {/* Optional: Scroll Indicator */}
       <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
-        <div className="w-6 h-10 border-2 border-white rounded-full flex items-start justify-center p-2">
+        <div className="w-6 h-10 border-2  rounded-full flex items-start justify-center p-2"
+        style={{color:theme.colors.secondary}}>
           <div 
             className="w-1 h-2 rounded-full"
-            style={{ backgroundColor: 'white' }}
+            style={{ backgroundColor: theme.colors.secondary }}
           />
         </div>
       </div>
